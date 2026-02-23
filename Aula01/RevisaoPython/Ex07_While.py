b1967c38c82224e319cc42cc3c83fa94c47da16f
André F S. <58055547+andreflores2009@users.noreply.github.com>
2026-02-23 20:05:59 -0300

    Create Ex07_While.py

---
+++ Aula01/RevisaoPython/Ex07_While.py
@@ -0,0 +1,13 @@
+"""
+Exemplo 7 - Laço de Repetição (while)
+Usa o laço while para repetir um bloco de código enquanto a condição for verdadeira.
+"""
+# inicializa a variável contador com 1
+contador = 1
+
+# enquanto o contador for menor ou igual a 5
+while contador <= 5:
+    # imprime o valor do contador
+    print("Contador:", contador)
+    # incrementa o contador em +1
+    contador += 1
