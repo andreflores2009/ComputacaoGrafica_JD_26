be80f878972e59908b3f94a13a6aa7f119d8275e
André F S. <58055547+andreflores2009@users.noreply.github.com>
2026-02-23 20:00:10 -0300

    Create Ex01_OlaMundo.py

---
+++ Aula01/RevisaoPython/Ex01_OlaMundo.py
@@ -0,0 +1,7 @@
+"""
+Exemplo 1 - Olá Mundo
+Este é o primeiro programa em Python, usado tradicionalmente como introdução.
+Ele apenas exibe uma mensagem na tela com o comando print().
+"""
+# imprime na tela a mensagem "Olá, mundo!"
+print("Olá, mundo!")
